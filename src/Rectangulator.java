00f2bd95c46300f65df10b0f758cfa20d5a55bda
root <root@luckypavan3.mylabserver.com>
2017-04-25 03:10:16 +0000

    changes to java code

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,6 +1,6 @@
 public class Rectangulator {
 	
-	public static void main (string[] args) {
+	public static void main (String[] args) {
 
 		int length = Integer.parseInt(args[0]);
 		int width = Integer.parseInt(args[1]);
