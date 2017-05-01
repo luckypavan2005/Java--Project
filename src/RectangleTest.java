651d9125e72b52a0a2b469bb09feaff4899beab1
root <root@luckypavan3.mylabserver.com>
2017-05-01 20:32:27 +0000

    added few more tests

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -28,6 +28,30 @@ import static org.junit.Assert.assertEquals;
 
                         assertEquals(myRectangle.width, 6);
                 }
+		
+		@Test
+                public void testGetArea() {
+
+                        assertEquals(myRectangle.getArea(), 30);
+                }
+
+                @Test
+                public void testGetPerimeter() {
+
+                        assertEquals(myRectangle.getPerimeter(), 22);
+                }
+
+                @Test
+                public void testLength() {
+
+                        assertEquals(myRectangle.length, 5);
+                }
+
+                @Test
+                public void testWidth() {
+
+                        assertEquals(myRectangle.width, 6);
+                }
 
 
 	}	
